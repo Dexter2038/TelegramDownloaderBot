100fcc0c77c4748566daddba6617f167da1d367c
Slender548 <barkaloff.m@gmail.com>
2024-06-15 01:27:28 +0300

    Added license

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import aiogram
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from os import environ
 from aiogram.utils.token import TokenValidationError
 import logging
@@ -9,7 +10,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-import aiogram.utils
 print(environ.get("TGBotDownloader"))
 try:
     bot = Bot(token=environ.get("TGBotDownloader"))
@@ -22,9 +22,18 @@ except Exception as e:
 
 dp = Dispatcher(bot=bot)
 
+
 @dp.message()
 async def youtube_search(message: Message):
     if not message.text.startswith("https://www.youtu"):
         await message.answer("Введите ссылку на видео из YouTube!")
         return
-    process = asyncio.create_subprocess_exec("yt-dlp", "-f bestvideo[ext=mp4]+bestaudio[ext=m4a]", "--skip-download", "--abort-on-error", message.text)
+    process = asyncio.create_subprocess_exec(
+        "yt-dlp",
+        "-f bestvideo[ext=mp4]+bestaudio[ext=m4a]",
+        "--skip-download",
+        "--abort-on-error",
+        "-O '%(requested_formats.0.height)s'",
+        message.text,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
